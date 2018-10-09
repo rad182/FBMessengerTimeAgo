dcbafa7d7c1a948eb878e2b5f0b1656551488929
Royce Dy <rad182@users.noreply.github.com>
2018-10-09 12:04:54 +0800

    Update NSDate+FBMessengerTimeAgo.swift

--- FBMessengerTimeAgo/NSDate+FBMessengerTimeAgo.swift
+++ FBMessengerTimeAgo/NSDate+FBMessengerTimeAgo.swift
@@ -6,34 +6,33 @@
 //  Copyright © 2016 Royce Dy. All rights reserved.
 //
 
-public extension NSDate {
+public extension Date {
 
     func formattedTimeAgo() -> String {
-        let calendar = NSCalendar.currentCalendar()
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.AMSymbol = "am"
-        dateFormatter.PMSymbol = "pm"
+        let calendar = NSCalendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.amSymbol = "am"
+        dateFormatter.pmSymbol = "pm"
 
         if calendar.isDateInToday(self) {
             dateFormatter.dateFormat = "h:mm a"
         } else if self.dayDifference() <= 6 {
             dateFormatter.dateFormat = "E"
-        } else if calendar.isDate(self, equalToDate: NSDate(), toUnitGranularity: .Year) {
+        } else if calendar.isDate(self, equalTo: Date(), toGranularity: .year) {
             dateFormatter.dateFormat = "MMM d"
         } else {
             dateFormatter.dateFormat = "MM/dd/yyyy"
         }
 
-        return dateFormatter.stringFromDate(self)
+        return dateFormatter.string(from: self)
     }
 
     private func dayDifference() -> Int {
-        let calendar = NSCalendar.currentCalendar()
+        let calendar = NSCalendar.current
+        let from = calendar.startOfDay(for: self)
+        let to = calendar.startOfDay(for: Date())
 
-        let from = calendar.startOfDayForDate(self)
-        let to = calendar.startOfDayForDate(NSDate())
-
-        let components = calendar.components([.Day], fromDate: from, toDate: to, options: [])
+        let components = calendar.dateComponents([.day], from: from, to: to)
 
         return components.day ?? 0
     }
